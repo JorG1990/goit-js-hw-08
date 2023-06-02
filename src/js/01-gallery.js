dcd53f1a700fe93bd32cb1fe36a339ede118adca
JorG1990 <jorge  garcia>
2023-06-01 23:06:21 -0500

    commit gallery 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery")
+
+const galleryImage = galleryItems.map (({preview,original,description}) =>
+
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`).join("");
+
+gallery.innerHTML= galleryImage;
+gallery.style.listStyle = "none";
+  const lightbox = new SimpleLightbox('.gallery a', {
+    animationSpeed: 250,
+    captionsData: "alt",
+  });
