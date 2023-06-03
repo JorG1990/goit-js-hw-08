40b4529ea81358782c890d4a29656a1d4d5569f7
JorG1990 <jorge  garcia>
2023-06-02 22:30:15 -0500

    segundo intento

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,28 @@
 
 import Player from '@vimeo/player';
 
-const player = new Player('handstick', {
-    id: "vimeo-player",
+const player = new Player('vimeo-player', {
+    id: 19231868,
     width: 640
 });
 
-player.on('play', function() {
-    console.log('played the video!');
+player.on('timeupdate', function (e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+  const timePlay = localStorage.getItem("videoplayer-current-time")
+  console.log(timePlay);
 });
+
+player.setCurrentTime(0).then((tiempoActual) => {
+   seconds = "videoplayer-current-time"
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+      default:
+          // some other error occurred
+          break;
+  }
+});
+
