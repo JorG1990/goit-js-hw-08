939d1914a9d1d7c163342e29b03136a2a7e46927
JorG1990 <jorge  garcia>
2023-06-06 21:10:51 -0500

    js 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,17 +14,13 @@ player.on('timeupdate',throttle(function (data) {
   console.log(timePlay);
 },1000));
 
-// Obtén la referencia al elemento de video
 const videoPlayer = document.getElementById('vimeo-player');
 
-// Verificar si hay un tiempo guardado al cargar la página
 window.addEventListener('load', () => {
   const savedTime = localStorage.getItem('videoplayer-current-time');
   if (savedTime) {
-    // Establecer el tiempo guardado en el reproductor de video
-    videoPlayer.currentTime = savedTime;
+       videoPlayer.currentTime = savedTime;
 
-    // Ejecutar la función de setCurrentTime con el tiempo guardado
     player.setCurrentTime(savedTime)
       .then(function (seconds) {
         console.log("El tiempo actual se estableció en " + seconds);
@@ -32,12 +28,12 @@ window.addEventListener('load', () => {
       .catch(function (error) {
         switch (error.name) {
           case 'RangeError':
-            // El tiempo era menor que 0 o mayor que la duración del video
+            // the time was less than 0 or greater than the video’s duration
             break;
           default:
-            // Ocurrió algún otro error
+            // some other error occurred
             break;
         }
-      });
-  }
+    });
+  }
 });
