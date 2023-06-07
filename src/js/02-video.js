817d7aa283e9efb5b21f4d58ac9f140f71f59131
JorG1990 <jorge  garcia>
2023-06-06 20:35:46 -0500

    js 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,43 @@
 
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 
 const player = new Player('vimeo-player', {
     id: 19231868,
     width: 640
 });
 
-player.on('timeupdate', function (e) {
-  localStorage.setItem('videoplayer-current-time', e.seconds);
+player.on('timeupdate',throttle(function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
   const timePlay = localStorage.getItem("videoplayer-current-time")
   console.log(timePlay);
+},1000));
+
+// Obtén la referencia al elemento de video
+const videoPlayer = document.getElementById('vimeo-player');
+
+// Verificar si hay un tiempo guardado al cargar la página
+window.addEventListener('load', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    // Establecer el tiempo guardado en el reproductor de video
+    videoPlayer.currentTime = savedTime;
+
+    // Ejecutar la función de setCurrentTime con el tiempo guardado
+    player.setCurrentTime(savedTime)
+      .then(function (seconds) {
+        console.log("El tiempo actual se estableció en " + seconds);
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            // El tiempo era menor que 0 o mayor que la duración del video
+            break;
+          default:
+            // Ocurrió algún otro error
+            break;
+        }
+      });
+  }
 });
-
-player.setCurrentTime(0).then((tiempoActual) => {
-   seconds = "videoplayer-current-time"
-}).catch(function(error) {
-  switch (error.name) {
-      case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-
-      default:
-          // some other error occurred
-          break;
-  }
-});
-
